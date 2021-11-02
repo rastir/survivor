2246a2cd350109d277401d0722aad7a1fb8861db
rasul <rastir@yandex.ru>
2021-11-02 22:00:04 +0300

    Лапоть - отправлено

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -109,7 +109,6 @@ namespace Level1Space
             public string str2 = "";
             public string Undostr = "";
             public int Undocommand = 0;
-//            public bool Cursor = false;
         }
 
         public static int Cursor = 0;
@@ -139,8 +138,6 @@ namespace Level1Space
             switch (burr)
             {
                 case 1:
-
-                    
                     Commands op = new Commands();
                     op.command = burr;
                     op.str = comanda;
@@ -149,7 +146,7 @@ namespace Level1Space
                     op.str2 = S;
                     int temp = Cursor;
                     Cursor = rrr.IndexOf(op);
-                    if (LastCursor)//(rrr.FindLast(t => t == t) != null && rrr.FindLast(t => t == t).command == 4)
+                    if (LastCursor)
                     {
                         for (int i = Cursor - 1; i > -1; i--)
                         {
@@ -162,48 +159,36 @@ namespace Level1Space
                     return S;
 
                 case 2:
-                    //try
-                    //{
-                        var second2 = Convert.ToInt16(comanda.Trim());
-                        Commands op2 = new Commands();
-                        op2.Undostr = S.Substring(S.Length - Math.Min(S.Length, second2));
-                        S = S.Remove(S.Length - Math.Min(S.Length, second2));
-                        op2.str2 = S;                   
-                        op2.command = burr;
-                        op2.str = comanda;
-                        rrr.Add(op2);
-                        int temp2 = Cursor;
-                        Cursor = rrr.IndexOf(op2);
-                        if (LastCursor)//(rrr.FindLast(t => t == t) != null && rrr.FindLast(t => t == t).command == 4)
-                        {
+                    var second2 = Convert.ToInt16(comanda.Trim());
+                    Commands op2 = new Commands();
+                    op2.Undostr = S.Substring(S.Length - Math.Min(S.Length, second2));
+                    S = S.Remove(S.Length - Math.Min(S.Length, second2));
+                    op2.str2 = S;                   
+                    op2.command = burr;
+                    op2.str = comanda;
+                    rrr.Add(op2);
+                    int temp2 = Cursor;
+                    Cursor = rrr.IndexOf(op2);
+                    if (LastCursor)
+                    {
                             
-                            for (int i = Cursor - 1; i > -1; i--)
-                            {
-                                if (i != temp2)
-                                rrr.RemoveAt(i);
-                            }
-                            Cursor = rrr.Count - 1;
+                        for (int i = Cursor - 1; i > -1; i--)
+                        {
+                            if (i != temp2)
+                            rrr.RemoveAt(i);
+                        }
+                        Cursor = rrr.Count - 1;
                     }
-                        LastCursor = false;
-                    //}
+                    LastCursor = false;
 
-                    //catch (Exception)
-                    //{
-
-                    //    throw;
-                    //}
                     return S;
 
                 case 3:
-                    //Commands op3 = new Commands();
-                    //op3.command = burr;
-                    //op3.str = comanda;
-                    //rrr.Add(op3);
+                    LastCursor = false;
                     if (Convert.ToInt32(comanda) > S.Length)
                         return "";
                     else
                         return S[Convert.ToInt32(comanda)].ToString();
-                    LastCursor = false;
 
                 case 4:
                     {
@@ -211,24 +196,8 @@ namespace Level1Space
 
                         if (last4 != null)
                         {
-                            //Commands op4 = new Commands();
-                            //if (last4.command == 1)
-                            //{
-                            //    if (S.EndsWith(last4.str))
-                            //        S = S.Substring(0, S.Length - last4.str.Length);
-                            //}
-                            //if (last4.command == 2)
-                            //{
-                            //    S += last4.Undostr;
-                            //}
-                            // op4.str = last4.Undostr;
-                            //op4.Undostr = last4.str;
-                            //op4.command = burr;
-                            //op4.Undocommand = last4.command;
-                            //last4.isUndo = true;
-                            //rrr.Add(op4);
                             LastCursor = true;
-                            //Cursor = rrr.IndexOf(op4);
+
                             if (Cursor - 1 >= 0)
                                 Cursor--;
                             S = rrr[Cursor].str2;
@@ -236,35 +205,11 @@ namespace Level1Space
                     }
                     return S;
                 case 5:
-                    //var last5 = rrr.FindLast(t => t.isUndo == false && t.command == 4);
-                    var last5 = rrr[Cursor]; //rrr.FindIndex(t => rrr[Cursor]);    //rrr.FindLast(t => rrr.IndexOf(t)=Cursor && ( t.command == 1 || t.command == 2));
+                    var last5 = rrr[Cursor]; 
                     if (last5 != null)
                     {
-                        //Commands op5 = new Commands();
-                        //if (last5.Undocommand == 1)
-                        //{
-                        //    S += last5.Undostr;
-                        //    op5.Undocommand = 0;
-                        //    op5.Undostr = "";
-                        //    op5.command = 1;
-                        //    op5.str = last5.Undostr;
-                        //}
-                        //if (last5.Undocommand == 2)
-                        //{
-                        //    if (S.EndsWith(last5.str))
-                        //        S = S.Substring(0, S.Length - last5.str.Length);
-                        //    op5.Undocommand = 0;
-                        //    op5.Undostr = "";
-                        //    op5.command = 2;
-                        //    op5.str = last5.Undostr;
-                        //}
-                        //last5.isUndo = true;
-                        //rrr.Add(op5);
-                        //if (rrr.Count - 1 <= rrr[Cursor] + 1)
-                        // Cursor = rrr[Cursor] + 1;
                         if (Cursor + 1 <= rrr.Count - 1 )
                             Cursor++;
-                        //S = rrr[Cursor].Undocommand.ToString() + " " + rrr[Cursor].Undostr.ToString();
                         S = rrr[Cursor].str2;
                         LastCursor = false;
                     }
