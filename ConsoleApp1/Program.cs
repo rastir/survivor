932b04e2a8edfde6dd5e0b0d8fa2f7b3ead72f63
Rasul <rastir@yandex.ru>
2021-11-25 13:58:46 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -102,15 +102,6 @@ namespace Level1Space
         }
         
         public static int summ;
-
-        static void Main()
-        {
-            //int[] A = { 1, 2, 1, 7, 2, 4, 3, 1, 5, 1, 2, 1, 6, 1, 2 }; //50233
-            //int N = 15;
-
-            //Console.WriteLine("результат: "+ Level1.TransformTransform(A, N));
-            //Console.ReadKey();
-        }
     }
 }
 #endregion
