82d01ea4192e9714ab13db41232c9b0cc8977974
rasul <rastir@yandex.ru>
2021-12-03 21:09:51 +0300

    Тренируем сборную России по футболу: попытка №2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -48,13 +48,66 @@ namespace Level1Space
             ///1. поменять местами два произвольных элемента: если предыдущий элемент больше следующего и таких ситуаций более одного - то нет
             ///2. изменить на обратный порядок произвольной последовательности: определить начало  и конец последовательности
             ///
-            return false;
+            int startindex = 0;
+            int endindex = 0;
+            bool true_startindex = false;
+
+            bool notOrder = false;
+            int[] F_copy = new int[N];
+            Array.Copy(F, 0, F_copy, 0, N);
+            int temp;
+
+            for (int i = 1; i < F.Length; i++)
+            {
+                if (F[i - 1] > F[i])
+                {
+                    if (startindex == 0 && true_startindex == false)
+                    {
+                        startindex = i - 1;
+                        true_startindex = true;
+                    }
+                }
+                if (i > 1 && F[i] > F[i - 1] && F[i - 2] > F[i - 1])
+                {
+                    if (endindex == 0)
+                        endindex = i - 1;
+                }
+            }
+            if (endindex == 0)
+                endindex = startindex + (N - (startindex + 1));
+            temp = F_copy[startindex];
+            F_copy[startindex] = F_copy[endindex];
+            F_copy[endindex] = temp;
+
+            //проверка
+            for (int i = 1; i < F_copy.Length; i++)
+            {
+                if (F_copy[i] < F_copy[i - 1])
+                    notOrder = true;
+            }
+            if (notOrder == false)
+                return true;
+            else
+            {
+                Array.Reverse(F, startindex,  endindex - startindex + 1);
+
+
+                for (int i = 1; i < F.Length; i++)
+                {
+                    if (F[i] < F[i - 1])
+                        notOrder = true;
+                }
+                if (notOrder == false)
+                    return true;
+                else 
+                    return false;
+            }
         }
         
         static void Main()
         {
-            int[] F = { 1, 3, 2};
-            int N = 3;
+            int[] F = { 9, 5, 3, 7, 1};
+            int N = 5;
 
             Console.WriteLine("результат: " + Level1.Football(F, N));
             Console.ReadKey();
--- Test_Level1/Test_Level1.cs
+++ Test_Level1/Test_Level1.cs
@@ -38,8 +38,74 @@ namespace Test_Level1
 
         public void Football2()
         {
-            int[] F = { 1, 3, 2 };
+            int[] F = { 3, 2, 1 };
             int N = 3;
+
+            try
+            {
+                Level1.Football(F, N);
+                Assert.IsTrue(Level1.Football(F, N));
+            }
+            catch (Exception)
+            {
+                Assert.Fail("TRUE");
+            }
+            finally
+            {
+                Console.WriteLine("результат: " + Level1.Football(F, N));
+            }
+        }
+        [TestMethod]
+        [TestCategory("28 задач")]
+
+        public void Football3()
+        {
+            int[] F = { 1, 7, 5, 3, 9 };
+            int N = 5;
+
+            try
+            {
+                Level1.Football(F, N);
+                Assert.IsTrue(Level1.Football(F, N));
+            }
+            catch (Exception)
+            {
+                Assert.Fail("TRUE");
+            }
+            finally
+            {
+                Console.WriteLine("результат: " + Level1.Football(F, N));
+            }
+        }
+        [TestMethod]
+        [TestCategory("28 задач")]
+
+        public void Football4()
+        {
+            int[] F = { 9, 5, 3, 7, 1 };
+            int N = 5;
+
+            try
+            {
+                Level1.Football(F, N);
+                Assert.IsFalse(Level1.Football(F, N));
+            }
+            catch (Exception)
+            {
+                Assert.Fail("False");
+            }
+            finally
+            {
+                Console.WriteLine("результат: " + Level1.Football(F, N));
+            }
+        }
+        [TestMethod]
+        [TestCategory("28 задач")]
+
+        public void Football5()
+        {
+            int[] F = { 1, 4, 3, 2, 5 };
+            int N = 5;
 
             try
             {
