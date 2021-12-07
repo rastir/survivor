6e91054957b3dbec71582a3b365216eb975a3f7a
Расул <rastir@yandex.ru>
2021-12-07 21:37:44 +0300

    Мастер ключей: попытка №1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -47,10 +47,43 @@ namespace Level1Space
             int[] Array_Logic = new int[k];
             for (int i = 0; i < Array_Logic.Length; i++)
             {
-                Array_Logic[i] = 0;//все двери закрыты
+                Array_Logic[i] = 1;//сразу открываем все двери
+            }
+            Console.WriteLine();
+            foreach (int a in Array_Logic)
+            {
+                Console.Write(Array_Logic[a] + " ");
             }
 
-            return "";
+            for (int i = 1; i < Array_Logic.Length; i++)
+            {
+                for (int j = 1; j < Array_Logic.Length; j++)
+                {
+                    if ((j+ 1) % (i + 1) == 0)
+                    {
+                        if (Array_Logic[j] == 0)
+                            Array_Logic[j] = 1;
+                        else
+                            Array_Logic[j] = 0;
+                    }
+                    Console.WriteLine();
+                    foreach (int a in Array_Logic)
+                    {
+                        Console.Write(a + " ");
+                    }
+                }
+                Console.WriteLine();
+                foreach (int a in Array_Logic)
+                {
+                    Console.Write(a + " ");
+                }
+            }
+            string s = "";
+            foreach (int i in Array_Logic)
+            {
+                s += i;
+            }
+            return s;
         }
         
         static void Main()
